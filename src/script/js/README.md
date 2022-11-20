7e96b7554336cd8e1867fa58cc7ed9d98a6c2dd2
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-20 20:29:28 +0900

    Create JS

---
+++ src/script/js/README.md
@@ -0,0 +1,14 @@
+# JS
+- scriptを動的に読み込む。
+
+## Usage
+
+### load
+```typescript
+window.js.load(1).then((constructor) => new constructor);
+```
+
+### get
+```typescript
+const constructor =window.js.get(1);
+```
---
+++ src/script/js/class.ts
@@ -0,0 +1,73 @@
+import { Component, InitOptions } from "../../component";
+
+type CacheMap = {
+  [key: string]: any
+}
+
+type ScriptId = number
+
+type Script = any;
+
+export class JS extends Component {
+  private caches: CacheMap = {}
+
+  constructor(options: InitOptions) {
+    super({
+      P: options.P,
+    });
+  }
+
+  get(id: ScriptId): Script {
+    return this.caches[id];
+  }
+
+  load(id: ScriptId): Promise<Script> {
+    return new Promise((resolve, reject) => {
+      if (this.caches[id]) {
+        resolve(this.caches[id]);
+      } else {
+        import("/scripts/" + id + ".js")
+          .then((module) => {
+            const moduleDefault = module.default;
+            const promises: Array<any> = [moduleDefault,];
+
+            if (moduleDefault) {
+              const scriptIds: Array<number> | undefined = moduleDefault.js;
+              const styleIds: Array<number> | undefined = moduleDefault.css;
+
+              scriptIds?.forEach((id) => promises.push(this.load(id)));
+              styleIds?.forEach((id) => promises.push(this.window!.css!.load(id)));
+            }
+
+            return Promise.all(promises);
+          })
+          .then((res) => {
+            const moduleDefault = res[0];
+
+            if (moduleDefault) {
+              let component;
+
+              if ("lostpetjp" === moduleDefault.type) {
+                component = moduleDefault.component;
+                this.caches[moduleDefault.id] = component;
+                this.window!.factory.create(component);
+              } else {
+                const constructor = component = moduleDefault.constructor;
+                component = this.caches[id] = constructor ? constructor : moduleDefault;
+              }
+
+              resolve(component);
+            }
+          })
+          .catch((err) => {
+            if (this.S) {
+              console.error(err);
+              this.window!.throw();
+            }
+
+            reject();
+          })
+      }
+    });
+  }
+}
