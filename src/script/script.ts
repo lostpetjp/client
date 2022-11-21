6cd3b21622c0d349a49582524e2e4dd79872194e
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-21 21:05:46 +0900

    Create script.ts

---
+++ src/script/script.ts
@@ -0,0 +1,44 @@
+import { Win } from "./window"
+import { Factory, features } from "./factory"
+import { DocumentData } from "./document"
+
+export type ServerOptions = {
+  document: DocumentData
+  version: number
+}
+
+(function () {
+  /**
+     * サーバーから受け取る情報
+     */
+  Object.setPrototypeOf(Win.prototype, features);
+
+  const win = new Win(self.a! as unknown as ServerOptions);
+
+  self["a"] = undefined;
+
+  if ("dev.lostpet.jp" === location.hostname) {
+    self["win"] = win;
+  }
+
+  /**
+   * localStorageに無駄なデータを残さない (Recaptchaなど)
+   */
+  for (var i = 0, len = localStorage.length; i < len; ++i) {
+    var keyName = localStorage.key(i);
+    if (keyName && keyName.length > 1) localStorage.removeItem(keyName);
+  }
+
+  /**
+   * preload用のlink要素を削除する
+   */
+  for (var i = 0, a = document.querySelectorAll("[rel='preload']"); a.length > i; i++) {
+    a[i].remove();
+  }
+
+  /**
+   * script要素を削除する
+   */
+  (document.currentScript as HTMLOrSVGScriptElement).remove();
+}());
+
