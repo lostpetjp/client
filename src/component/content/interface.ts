fe0f26cf7d4dc9a096ba2b5130273d28829d25e0
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-22 17:37:55 +0900

    Update interface.ts

--- src/component/content/interface.ts
+++ src/component/content/interface.ts
@@ -1,12 +1,9 @@
 import { Component } from ".."
-import { StyleIdList } from "../../script/css"
 import { ContentData, DocumentItem } from "../../script/document"
 
 export interface Content extends Component {
   P?: DocumentItem
   id: number
-
-  css: StyleIdList
 
   ready?(): Promise<void>
   create(data: ContentData): Promise<void> | void
