b9e2ae6ca7d1cd6ae173506d97c0a80f9ee4d916
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-23 20:29:24 +0900

    Create recaptcha class

---
+++ src/component/0/3/class.ts
@@ -0,0 +1,102 @@
+import { Component, InitOptions } from "../..";
+
+export type Grecaptcha = {
+  ready: (callback: () => void) => void
+  render: (element: HTMLDivElement, options: {
+    callback: (code: string) => void,
+    "error-callback": () => void,
+    "expired-callback": () => void,
+    sitekey: string
+    size: "compact" | "normal"
+    theme: "dark" | "light"
+  }) => number
+  reset: (id: number) => void
+}
+
+declare var grecaptcha: Grecaptcha;
+
+export type RecaptchaOptions = InitOptions & {
+  element: HTMLDivElement
+}
+
+type RecaptchaValue = null | string
+type RecaptchaId = null | number
+
+/**
+ * 指定した要素にRecaptchaを配置する
+ */
+export class Recaptcha extends Component {
+  private id: RecaptchaId = null
+  private readonly siteKey: string = "[SITE KEY]"
+
+  // value
+  private $value: RecaptchaValue = null
+
+  set value(value: RecaptchaValue) {
+    this.$value = value;
+  }
+
+  get value(): RecaptchaValue {
+    return this.$value;
+  }
+
+  reset(): void {
+    this.value = null;
+    if (null !== this.id) grecaptcha.reset(this.id);
+  }
+
+  constructor(options: RecaptchaOptions) {
+    super({
+      P: options.P,
+    });
+
+    const element = options.element;
+
+    var scriptE = document.createElement("script");
+
+    (new Promise<void>((resolve) => {
+      scriptE.src = "//www.google.com/recaptcha/api.js";
+      document.body.appendChild(scriptE);
+
+      var timeouts = this.T;
+
+      timeouts[0] = setInterval(() => {
+        if (this.S) {
+          if ("grecaptcha" in self) {
+            clearInterval(this.T[0]);
+            resolve();
+          }
+        }
+      }, 8);
+    }))
+      .then(() => {
+        if (this.S) {
+          grecaptcha.ready(() => {
+            if (this.S) {
+              var win = this.window!;
+              var errorEv = () => (this.value = null);
+
+              this.id = grecaptcha.render(element, {
+                callback: (code) => (this.value = code),
+                "error-callback": errorEv,
+                "expired-callback": errorEv,
+                sitekey: this.siteKey,
+                size: 360 > win.innerWidth ? "compact" : "normal",
+                theme: (document.documentElement.classList.contains("t2")) ? "dark" : "light",
+              });
+            }
+          });
+        }
+      })
+      .catch((err) => {
+        if (this.S) {
+          console.error(err);
+          this.window!.throw();
+        }
+      });
+
+    this.on!(this, "destroy", () => {
+      scriptE.remove();
+    });
+  }
+}
---
+++ src/component/0/3/index.ts
@@ -0,0 +1,9 @@
+import { Recaptcha } from "./class"
+
+export default {
+  id: 3,
+  css: [16, 17,],
+  js: [],
+  type: "lostpetjp",
+  component: Recaptcha,
+}
