5de37749ba78e26e3d3e492f444eed030f4b2503
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:35:09 +0900

    Update class.ts

--- src/script/window/class.ts
+++ src/script/window/class.ts
@@ -6,6 +6,9 @@ import { DocumentM, DocumentItem } from "../document"
 import { Me } from "../me"
 import { Json2Node } from "../element"
 import { ServerOptions } from "../script"
+import { Dialog } from "../dialog"
+import { Popup, PopupLayer } from "../popup"
+import { Drawer } from "../drawer"
 
 export type FetchOptions = {
   credentials: boolean
@@ -32,6 +35,10 @@ export class Win extends Component {
       Json2Node,
       DocumentM,
       DocumentItem,
+      Dialog,
+      Drawer,
+      Popup,
+      PopupLayer,
     ].forEach((constructor) => {
       this.factory.create(constructor);
     });
@@ -39,9 +46,11 @@ export class Win extends Component {
     this.css = new CSS({ P: this, default: options.css });
     this.me = new Me({ P: this, });
     this.js = new JS({ P: this, });
-    this.document = new DocumentM({ P: this });
-
+    this.drawer = new Drawer({ P: this });
+    this.dialog = new Dialog({ P: this });
+    this.popup = new Popup({ P: this });
     this.element = new Json2Node;
+    this.document = new DocumentM({ P: this });
 
     // for popstate
     addEventListener("popstate", (event) => {
@@ -54,14 +63,26 @@ export class Win extends Component {
       passive: false,
     })
 
-    this.css!.attach(this, [3, 4, 5, 6, 10, 11, 12, 13, 14,]);
+    this.css.attach(this, 3);
+
+    // for reduce-motion
+    matchMedia("(prefers-reduced-motion)").addEventListener("change", (event: MediaQueryListEvent) => {
+      const reduceMode = localStorage.getItem("r");
+      var isReduce = "2" === reduceMode || ("1" !== reduceMode && event.matches);
+      document.documentElement.classList[isReduce ? "add" : "remove"]("r1");
+      this.css.build();
+    }, {
+      passive: true,
+    });
 
     // for color-scheme
-    matchMedia("(prefers-color-scheme:dark)").addEventListener("change", (event) => {
+    matchMedia("(prefers-color-scheme:dark)").addEventListener("change", (event: MediaQueryListEvent) => {
       const colorMode = localStorage.getItem("t");
       var isDark = "2" === colorMode || ("1" !== colorMode && event.matches);
       document.documentElement.classList.replace(isDark ? "t1" : "t2", isDark ? "t2" : "t1");
-      this.css!.build();
+      this.css.build();
+    }, {
+      passive: true,
     });
 
     // for resize
@@ -69,7 +90,7 @@ export class Win extends Component {
       this.innerHeight = innerHeight;
       this.innerWidth = innerWidth;
 
-      this.emit!("resize");
+      // this.emit!("resize"); // 未使用
     };
 
     addEventListener("resize", resizeListener, {
@@ -164,9 +185,12 @@ export class Win extends Component {
   factory: Factory = new Factory
   version: number = 0
 
-  css?: CSS
-  document?: DocumentM
-  element?: Json2Node
-  me?: Me
-  js?: JS
+  css: CSS
+  dialog: Dialog
+  drawer: Drawer
+  document: DocumentM
+  element: Json2Node
+  me: Me
+  js: JS
+  popup: Popup
 }
