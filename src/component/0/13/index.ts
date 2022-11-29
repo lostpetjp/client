7e347be7cd67bc9071c25968e4730f46738a1f08
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 17:43:14 +0900

    Update index.ts

--- src/component/0/13/index.ts
+++ src/component/0/13/index.ts
@@ -1,6 +1,7 @@
 import { FromValidityManager, InputValidityManager, RecaptchaValidityManager } from "./class"
 
 export default {
+  css: [23,],
   js: [3,],
   type: "lostpetjp",
   component: FromValidityManager,
