759f23a291559669ed63c239359064956f5de09c
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:35:48 +0900

    Create class.ts

---
+++ src/script/dialog/class.ts
@@ -0,0 +1,44 @@
+import { Component, InitOptions } from "../../component";
+import { DialogItem, DialogItemOptions } from "../../component/0/5/class";
+
+type DialogOptions = InitOptions & {
+};
+
+export class Dialog extends Component {
+  private readonly items: Array<DialogItem> = []
+
+  constructor(options: DialogOptions) {
+    super({
+      P: options.P,
+    });
+  }
+
+  /**
+   * Create dialog.
+   */
+  create(options: DialogItemOptions): Promise<DialogItem> {
+    this.close();
+
+    return new Promise((resolve, reject) => {
+      const win = this.window!;
+      const js = win.js!;
+
+      js.load(5)
+        .then((constructor: typeof DialogItem) => {
+          var newItem = new constructor(options);
+
+          this.items.push(newItem);
+
+          resolve(newItem);
+        })
+        .finally(reject);
+    });
+  }
+
+  close() {
+    for (var i = 0, a = this.items; a.length > i; i++) {
+      a[i].close();
+      this.items.splice(i--, 1);
+    }
+  }
+}
