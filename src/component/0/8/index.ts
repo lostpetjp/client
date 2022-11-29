32102236233f63696097b1697b9afce49bd2ab74
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:36:49 +0900

    Create index.ts

---
+++ src/component/0/8/index.ts
@@ -0,0 +1,8 @@
+import { DrawerItemLeft, DrawerItemLeftStyleIdList } from "./class"
+
+export default {
+  css: DrawerItemLeftStyleIdList,
+  id: 8,
+  type: "lostpetjp",
+  component: DrawerItemLeft,
+}
