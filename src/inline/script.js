6eb0ebde8e9a718f2e618cc50880e413fa15c50b
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:35:20 +0900

    Update script.js

--- src/inline/script.js
+++ src/inline/script.js
@@ -5,4 +5,11 @@ if ("2" === t || ("1" !== t && matchMedia("(prefers-color-scheme:dark)").matches
   document.documentElement.classList.replace("t1", "t2");
 }
 
+// 視差効果の反映
+var t = localStorage.getItem("r");
+
+if ("2" === t || ("1" !== t && matchMedia("(prefers-reduced-motion)").matches)) {
+  document.documentElement.classList.add("r2");
+}
+
 document.currentScript.remove();
