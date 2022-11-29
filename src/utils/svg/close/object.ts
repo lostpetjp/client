c9115079a8ef64fc009b4eba274f2d6aeb8ee4bc
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:34:35 +0900

    Create object.ts

---
+++ src/utils/svg/close/object.ts
@@ -0,0 +1,13 @@
+export const SVGCloseElementJSON = {
+  attribute: {
+    viewBox: "0 0 48 48",
+  },
+  children: {
+    attribute: {
+      d: "M3.692 48 0 44.308 20.308 24 0 3.692 3.692 0 24 20.308 44.308 0 48 3.692 27.692 24 48 44.308 44.308 48 24 27.692 3.692 48Z",
+      fill: "currentColor",
+    },
+    tagName: "path",
+  },
+  tagName: "svg",
+}
