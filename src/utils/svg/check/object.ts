cfda6be779dcf630eaf5447f08a13a372a3f44d7
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:35:03 +0900

    Create object.ts

---
+++ src/utils/svg/check/object.ts
@@ -0,0 +1,13 @@
+export const SVGCheckElementJSON = {
+  attribute: {
+    viewBox: "0 0 48 48",
+  },
+  children: {
+    attribute: {
+      d: "M16.516 41.277 0 24.761l3.171-3.17 13.345 13.345L44.829 6.623 48 9.793 16.516 41.277Z",
+      fill: "currentColor",
+    },
+    tagName: "path",
+  },
+  tagName: "svg",
+}
