d2ed009b02c08ed1df395027ee565a751b5416a9
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:36:53 +0900

    Create class.ts

---
+++ src/component/0/7/class.ts
@@ -0,0 +1,14 @@
+import { Component, InitOptions } from "../..";
+import { DrawerCloseOptions } from "../../../script/drawer";
+
+export type DrawerItemFullpageOptions = InitOptions & {
+
+}
+
+export const DrawerItemFullpageStyleIdList = []
+
+export class DrawerItemFullpage extends Component {
+  close(options?: DrawerCloseOptions): void {
+
+  }
+}
