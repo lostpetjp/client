e18f3f809235a2894d53fd0fc45957c9d3cbb964
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:34:28 +0900

    Create object.ts

---
+++ src/utils/svg/drawer/object.ts
@@ -0,0 +1,13 @@
+export const SVGDrawerElementJSON = {
+  attribute: {
+    viewBox: "0 0 48 48",
+  },
+  children: {
+    attribute: {
+      d: "M0 40v-4h48v4H0Zm0-14v-4h48v4H0Zm0-14V8h48v4H0Z",
+      fill: "currentColor",
+    },
+    tagName: "path",
+  },
+  tagName: "svg",
+}
