2cce044eb980a9cc3f09801f00e39af1122acd84
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 14:31:59 +0900

    Update class.ts

--- src/script/js/class.ts
+++ src/script/js/class.ts
@@ -23,57 +23,66 @@ export class JS extends Component {
     return this.caches[id];
   }
 
-  load(id: ScriptId): Promise<Script> {
-    return new Promise((resolve, reject) => {
-      if (this.caches[id]) {
-        resolve(this.caches[id]);
-      } else {
-        import("/scripts/" + id + ".js?v=" + this.window!.version)
-          .then((module) => {
-            const moduleDefault = module.default;
-            const promises: Array<any> = [moduleDefault,];
+  load(ids: ScriptIdList | ScriptId): Promise<Script> {
+    if (!Array.isArray(ids)) ids = [ids,];
 
-            if (moduleDefault) {
-              const scriptIds: Array<number> | undefined = moduleDefault.js;
-              const styleIds: Array<number> | undefined = moduleDefault.css;
+    return Promise.all(ids.map((id) => {
+      return new Promise((resolve, reject) => {
+        if (this.caches[id]) {
+          resolve(this.caches[id]);
+        } else {
+          import("/scripts/" + id + ".js?v=" + this.window!.version)
+            .then((module) => {
+              const moduleDefault = module.default;
+              const promises: Array<any> = [moduleDefault,];
 
-              scriptIds?.forEach((id) => promises.push(this.load(id)));
-              if (styleIds) promises.push(this.window!.css!.load(styleIds));
-            }
+              if (moduleDefault) {
+                const scriptIds: Array<number> | undefined = moduleDefault.js;
+                const styleIds: Array<number> | undefined = moduleDefault.css;
 
-            return Promise.all(promises);
-          })
-          .then((res) => {
-            const moduleDefault = res[0];
-
-            if (moduleDefault) {
-              let component;
-
-              if ("lostpetjp" === moduleDefault.type) {
-                component = moduleDefault.component;
-                this.caches[moduleDefault.id] = component;
-                this.window!.factory.create(component);
-
-                moduleDefault.plugins?.forEach((component: ExtendsTarget) => {
-                  this.window!.factory.create(component);
-                });
-              } else {
-                const constructor = component = moduleDefault.constructor;
-                component = this.caches[id] = constructor ? constructor : moduleDefault;
+                scriptIds?.forEach((id) => promises.push(this.load(id)));
+                if (styleIds) promises.push(this.window!.css!.load(styleIds));
               }
 
-              resolve(component);
-            }
-          })
-          .catch((err) => {
-            if (this.S) {
-              console.error(err);
-              this.window!.throw();
-            }
+              return Promise.all(promises);
+            })
+            .then((res) => {
+              const moduleDefault = res[0];
 
-            reject();
-          })
-      }
-    });
+              if (moduleDefault) {
+                let component;
+                const type = moduleDefault.type;
+
+                if ("lostpetjp" === type) {
+                  component = moduleDefault.component;
+                  this.caches[id] = component;
+                  this.window!.factory.create(component);
+
+                  moduleDefault.plugins?.forEach((component: ExtendsTarget) => {
+                    this.window!.factory.create(component);
+                  });
+                } else if ("function" === type) {
+                  component = this.caches[id] = moduleDefault.component;
+
+                } else {
+                  throw "error!!!!!!!TODO";
+                  const constructor = component = moduleDefault.constructor;
+                  component = this.caches[id] = constructor ? constructor : moduleDefault;
+                }
+
+                resolve(component);
+              }
+            })
+            .catch((err) => {
+              if (this.S) {
+                console.error(err);
+                this.window!.throw();
+              }
+
+              reject();
+            })
+        }
+      });
+    }));
   }
 }
