4eb8112890288c9cc6a4bd7035d456dc42892daa
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:35:11 +0900

    Update script.ts

--- src/script/script.ts
+++ src/script/script.ts
@@ -9,38 +9,36 @@ export type ServerOptions = {
   version: number
 }
 
-(function () {
-  /**
-     * サーバーから受け取る情報
-     */
-  Object.setPrototypeOf(Win.prototype, features);
-
-  const win = new Win(self.a! as unknown as ServerOptions);
-
-  self["a"] = undefined;
-
-  if ("dev.lostpet.jp" === location.hostname) {
-    self["win"] = win;
-  }
-
-  /**
-   * localStorageに無駄なデータを残さない (Recaptchaなど)
+/**
+   * サーバーから受け取る情報
    */
-  for (var i = 0, len = localStorage.length; i < len; ++i) {
-    var keyName = localStorage.key(i);
-    if (keyName && keyName.length > 1) localStorage.removeItem(keyName);
-  }
+Object.setPrototypeOf(Win.prototype, features);
 
-  /**
-   * preload用のlink要素を削除する
-   */
-  for (var i = 0, a = document.querySelectorAll("[rel='preload']"); a.length > i; i++) {
-    a[i].remove();
-  }
+const win = new Win(self.a! as unknown as ServerOptions);
 
-  /**
-   * script要素を削除する
-   */
-  (document.currentScript as HTMLOrSVGScriptElement).remove();
-}());
+self["a"] = undefined;
+
+if ("dev.lostpet.jp" === location.hostname) {
+  self["win"] = win;
+}
+
+/**
+ * localStorageに無駄なデータを残さない (Recaptchaなど)
+ */
+for (var i = 0, len = localStorage.length; i < len; ++i) {
+  var keyName = localStorage.key(i);
+  if (keyName && keyName.length > 1) localStorage.removeItem(keyName);
+}
+
+/**
+ * preload用のlink要素を削除する
+ */
+for (var i = 0, a = document.querySelectorAll("[rel='preload']"); a.length > i; i++) {
+  a[i].remove();
+}
+
+/**
+ * script要素を削除する
+ */
+(document.currentScript as HTMLOrSVGScriptElement).remove();
 
