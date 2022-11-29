8e8de79d1b52913b409f601fa2c07646e1bdece8
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:34:41 +0900

    Create object.ts

---
+++ src/utils/svg/chevron-down/object.ts
@@ -0,0 +1,13 @@
+export const SVGChevronDownElementJSON = {
+  attribute: {
+    viewBox: "0 0 48 48",
+  },
+  children: {
+    attribute: {
+      d: "M2.484 5.699 12 15.215l9.516-9.516a1.456 1.456 0 0 1 2.058 2.057L13.029 18.301a1.455 1.455 0 0 1-2.058 0L.426 7.756a1.455 1.455 0 0 1 2.058-2.057Z",
+      fill: "currentColor",
+    },
+    tagName: "path",
+  },
+  tagName: "svg",
+}
