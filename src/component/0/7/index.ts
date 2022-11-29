3ea9e84857bcc2a7d3f0bd12de14a636c22c3b0e
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-29 09:36:51 +0900

    Create index.ts

---
+++ src/component/0/7/index.ts
@@ -0,0 +1,8 @@
+import { DrawerItemFullpage, DrawerItemFullpageStyleIdList } from "./class"
+
+export default {
+  css: DrawerItemFullpageStyleIdList,
+  id: 7,
+  type: "lostpetjp",
+  component: DrawerItemFullpage,
+}
