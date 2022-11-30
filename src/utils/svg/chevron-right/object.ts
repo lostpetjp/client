f19cce43a73d039cc82c7d2d69ce5f345ee39179
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-11-30 22:57:57 +0900

    Create object.ts

---
+++ src/utils/svg/chevron-right/object.ts
@@ -0,0 +1,13 @@
+export const SVGChevronRightElementJSON = {
+  attribute: {
+    viewBox: "0 0 48 48",
+  },
+  children: {
+    attribute: {
+      d: "m13.841 48-4.282-4.282L29.277 24 9.559 4.282 13.841 0l24 24-24 24Z",
+      fill: "currentColor",
+    },
+    tagName: "path",
+  },
+  tagName: "svg",
+}
