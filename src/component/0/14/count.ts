3a84ff7a5e1b66885c2e1cda254be582b40f4599
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-02 23:04:06 +0900

    Create count.ts

---
+++ src/component/0/14/count.ts
@@ -0,0 +1,102 @@
+import { Component, InitOptions } from "../.."
+import { SearchAnimalId } from "../../../types/animal"
+import { SearchMatterId } from "../../../types/matter"
+import { SearchPrefectureId } from "../../../types/prefecture"
+import { SearchUrlObject } from "../16/class"
+import { SearchLocationObject, SearchTemplate } from "./class"
+
+type Options = InitOptions & {
+  element?: HTMLDivElement | null
+}
+
+export class SearchCount extends Component {
+  element: HTMLDivElement
+
+  dom: Array<HTMLSpanElement | HTMLAnchorElement> = []
+
+  constructor(options: Options) {
+    super({
+      P: options.P,
+    });
+
+    const win = this.window!;
+    const element = win.element;
+    let rootE, backE;
+
+    if (options.element) {
+      rootE = this.element = options.element as HTMLDivElement;
+      const childNodes = rootE.childNodes;
+
+      this.dom = [
+        childNodes[0] as HTMLSpanElement,
+      ];
+
+      backE = childNodes[2];
+
+    } else {
+      rootE = this.element = element.create({
+        attribute: {
+          class: "c25d",
+        },
+        children: [
+          this.dom[0] = element.create({
+            attribute: {
+              class: "c25d1",
+            },
+            children: "",
+            tagName: "span",
+          }) as HTMLSpanElement,
+          "件ヒットしました。",
+        ],
+        tagName: "div",
+      }) as HTMLDivElement;
+    }
+
+    this.dom[1] = backE as HTMLSpanElement | null || element.create({
+      children: [
+        "(",
+        {
+          attribute: {
+            class: "a1",
+            href: "/search/",
+          },
+          children: "全部見る",
+          tagName: "a",
+        },
+        ")",
+      ],
+      tagName: "span",
+    }) as HTMLSpanElement;
+
+    this.dom[2] = this.dom[1].getElementsByTagName("a")[0];
+  }
+
+  update(object: SearchLocationObject, counts: Array<number>): void {
+    const doms = this.dom;
+    const matterId = object.matter;
+    const hasSearch = matterId || object.animal || object.prefecture;
+
+    (doms[0].firstChild as Text).data = ("" + counts[matterId]).replace(/(\d)(?=(\d{3})+\b)/g, "$1,");
+
+    const backE = doms[1];
+    const isContains = backE.parentNode;
+
+    if (hasSearch && !isContains) {
+      (doms[2] as HTMLAnchorElement).href = (this.window!.js.get(16) as SearchUrlObject).create({ ...object, matter: 0 as SearchMatterId, animal: 0 as SearchAnimalId, prefecture: 0 as SearchPrefectureId, page: 1, }, this.P! as SearchTemplate);
+
+      this.element.appendChild(backE);
+
+    } else if (!hasSearch && isContains) {
+      backE.remove();
+
+    }
+  }
+}
+
+/*
+<div class="c25d">
+  <span class="c25d1">1,332</span>
+  件ヒットしました。
+  <span>(<a class="a1" href="/search/">全部見る</a>)</span>
+</div>
+*/
