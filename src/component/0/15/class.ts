ed86660ba21a2135268c43a0806121a638de5034
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-02 18:59:40 +0900

    Create class.ts

---
+++ src/component/0/15/class.ts
@@ -0,0 +1,48 @@
+import { Component } from "../../../component"
+import { DocumentItem, ContentData } from "../../../script/document"
+import { Content } from "../../content"
+export type SearchContentData = {
+  title: string
+  body: any
+
+  matter: number
+  animal: number
+  prefecture: number
+  sort: number
+  page: number
+  count: Array<number>
+
+  items?: Array<any>
+} & ContentData;
+
+export class SearchContent extends Component implements Content {
+  P?: DocumentItem
+
+  title?: string
+  element?: Node
+
+  //matter?: number
+  //animal?: number
+  //prefecture?: number
+  //sort?: number
+  //page?: number
+
+  items?: Array<any>
+  count?: Array<number>
+
+  create(data: SearchContentData): void {
+    console.log("search content", data);
+
+    this.title = data.title;
+
+    // this.matter = data.matter;
+    // this.animal = data.animal;
+    // this.prefecture = data.prefecture;
+    // this.sort = data.sort;
+    // this.page = data.page;
+    this.items = data.items;
+    this.count = data.count;
+
+    this.element = this.window!.element!.create(data.body);
+  }
+}
