fa0816a8dbf19fbcfaddf27965187984092c8b22
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-09 17:21:58 +0900

    Create types.ts

---
+++ src/component/0/29/types.ts
@@ -0,0 +1,30 @@
+type Options = {
+  arrowKeys: boolean
+  clickToCloseNonZoomable: boolean,
+  closeOnVerticalDrag: boolean,
+  escKey: boolean,
+  loop: boolean,
+  showHideAnimationType: "none"
+  pswpModule: PhotoSwipe
+  returnFocus: boolean
+}
+
+export class PhotoSwipeLightbox {
+  constructor(options: Options) {}
+
+  init(){}
+
+  on(name: string, callback: (event: {[key: string]: any}) => void) {}
+
+  loadAndOpen(index: number) {}
+
+  destroy() {}
+}
+
+export class PhotoSwipe {
+  constructor(options: Options) {}
+}
+
+export class PhotoSwipeVideo {
+  constructor(photoSwipe: PhotoSwipe, options: {}) {}
+}
