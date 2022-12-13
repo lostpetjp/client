201977722f7205d4300f1927fb999a7141fb06d4
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-13 23:52:10 +0900

    Create object.ts

---
+++ src/utils/svg/menu/object.ts
@@ -0,0 +1,13 @@
+export const SVGMenuElementJSON = {
+  attribute: {
+    viewBox: "0 0 24 24",
+  },
+  children: {
+    attribute: {
+      d: "M3 9c-1.65 0-3 1.35-3 3s1.35 3 3 3 3-1.35 3-3-1.35-3-3-3Zm18 0c-1.65 0-3 1.35-3 3s1.35 3 3 3 3-1.35 3-3-1.35-3-3-3Zm-9 0c-1.65 0-3 1.35-3 3s1.35 3 3 3 3-1.35 3-3-1.35-3-3-3Z",
+      fill: "currentColor",
+    },
+    tagName: "path",
+  },
+  tagName: "svg",
+}
