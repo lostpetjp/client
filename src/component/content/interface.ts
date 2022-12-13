2a9e8f6db9d89a8a2ea9aead6cb4e9814eb714f5
lostpet.jp <38132078+lostpetjp@users.noreply.github.com>
2022-12-13 23:52:35 +0900

    Update interface.ts

--- src/component/content/interface.ts
+++ src/component/content/interface.ts
@@ -1,9 +1,17 @@
 import { Component } from ".."
 import { ContentData, DocumentItem } from "../../script/document"
 
+export type ContentOptions = {
+  P: Component
+  pathname: string
+  search: string
+}
+
 export interface Content extends Component {
   P?: DocumentItem
   id?: number
+  pathname: string
+  search: string
 
   ready?(): Promise<void>
   create(data: ContentData): Promise<void> | void
